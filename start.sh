6adc38e104fdc032ac87c8813176c5aaf2d296f6
Laggy_Mag <69040612+Ha2k6@users.noreply.github.com>
2025-02-16 14:15:24 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-
-# Start the Flask app
-gunicorn -b 0.0.0.0:5000 app:app
-￼Enter
